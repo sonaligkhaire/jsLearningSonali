08558ac0a46fdb277af3f8ea2330870b76e801a1
sonaligkhaire <sonaligkhaire601@gmail.com>
2023-07-19 18:41:12 +0530

    commit my  1st js file assignment  with console declaration and initialization
    commit my  1st  js file assignment  with console declaration and initialization

---
+++ 01_variableAssign.js
@@ -0,0 +1,32 @@
+
+var myFullName;
+console.log(myFullName);
+myFullName ="My Name is: Sonali Khaire";
+console.log(myFullName);
+myFullName ="My full name is: sonali vikas khaire";
+console.log(myFullName);
+
+var myLove;
+myLove = "I love only JS";
+console.log(myLove);
+
+var firstName ="My First Name is: Sonali"; // declaration and initialization on same line
+console.log(firstName);
+
+var lastName ="My Last Name is: Khaire";
+console.log(lastName);
+
+var age ="Age is: 26";
+console.log(age);
+
+var address ="My Address is: Pune";
+console.log(address);
+
+var pinCode =410505;
+console.log(pinCode);
+
+var gender ="Gender is: Female";
+console.log(gender);
+
+var isMarried ="is Married: yes";
+console.log(isMarried);
