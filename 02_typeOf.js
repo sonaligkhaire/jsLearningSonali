08481a442fc5c3d5e1fd092f8d8a43529b4add2f
sonaligkhaire <sonaligkhaire601@gmail.com>
2023-07-19 18:41:45 +0530

    commit 2nd assignment of js file
    commit 2nd assignment of js file

---
+++ 02_typeOf.js
@@ -0,0 +1,23 @@
+
+var bankName;
+console.log(bankName);
+bankName ="Bank Name is: SBI Bank";
+console.log(bankName);
+console.log(typeof bankName);
+
+var marks = "Mark is :90%";
+console.log(marks);
+console.log(typeof marks);
+
+var isWorking;
+console.log(isWorking);
+isWorking = true;
+console.log(typeof isWorking);
+
+var totalCount = "Hundred and Seven";
+console.log(totalCount);
+console.log(typeof totalCount);
+totalCount = 107;
+console.log(totalCount);
+console.log(typeof totalCount);
+
