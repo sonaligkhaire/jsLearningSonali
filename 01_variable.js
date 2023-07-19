01e678bd4542c4606a7949396b2b808e5e578052
sonaligkhaire <sonaligkhaire601@gmail.com>
2023-07-19 07:50:30 +0530

    basic programming
    basic programming

---
+++ 01_variable.js
@@ -0,0 +1,10 @@
+
+var myName;//This is my variable declaration
+myName = "Sonali";//variable assignment or initialization
+console.log(myName);
+
+var  city = "Pune";//variable assignment or initialization
+console.log(city);
+
+var pin ; //variable declaration
+console.log(pin);
