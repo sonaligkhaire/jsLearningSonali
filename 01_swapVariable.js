81bd9be8ac20ac9618d9b1da11c6bfce1b0ed76a
sonaligkhaire <sonaligkhaire601@gmail.com>
2023-07-20 14:58:59 +0530

    added my first swap datatypes assignment
    added my first swap datatypes assignment

---
+++ 01_swapVariable.js
@@ -0,0 +1,22 @@
+
+console.log('-----------------------------------1st Swap------------------------------------------------');
+var sweety = "Sweety";
+var cutie = "Cutie";
+console.log('Before Swap:  value of sweety is:-',sweety , 'And'  , 'value of cutie is:- ', cutie);
+var temp = sweety
+var sweety = cutie;
+var cutie = temp;
+console.log('After Swap :   value of sweety is:-',sweety , 'And' ,'value of cutie is:- ', cutie);
+
+console.log('----------------------------------2nd Swap-------------------------------------------------');
+
+var num1 = 100; //200
+var num2 = 200;  //300
+var num3 = 300;   //100
+console.log('Before Swap:  value of num1 is:-', num1, 'value of num2 is:-', num2,'And',  'value of num3 is:-', num3);
+var temp = num1; 
+var num1 = num2;
+var num2 = num3;
+var num3 = temp;
+console.log('After Swap :  value of num1 is:-', num1,'value of num2 is:-', num2, 'And',  'value of num3 is:-', num3);
+
