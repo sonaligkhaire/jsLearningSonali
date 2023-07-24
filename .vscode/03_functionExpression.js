97b06db593b9a1b463a32fe7a45324ae84ce9758
sonaligkhaire <sonaligkhaire601@gmail.com>
2023-07-24 08:44:10 +0530

    added function expressions
    added function expressions

---
+++ .vscode/03_functionExpression.js
@@ -0,0 +1,27 @@
+//function expression
+
+var num = 100;
+console.log(num);
+var display = function(){
+    console.log(`I am Angular Developer !`);
+}
+display();
+console.log(typeof display);
+
+var add = function(num1 ,num2 ){
+    // console.log(`Addition is: ${num+num2}`);
+    var result = num1+num2;
+    console.log(`Addition is:${result}`); //argument no return value
+
+
+}
+add(20,40);
+
+var wordCount = function(word){  //word is argument/parameter
+    var words = word.split(" ");
+     var wordCount= words.length;
+    console.log(`In given String - ${word}`);
+    return wordCount;
+}
+var result = wordCount("I am UI Developer");
+console.log(`Total word count is: ${result}`);//function expression with argument and return value
