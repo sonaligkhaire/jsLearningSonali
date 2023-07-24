b31f0f6eae6bbe1cb7c6147164eb53a18e58dbe4
sonaligkhaire <sonaligkhaire601@gmail.com>
2023-07-24 08:42:30 +0530

    added operators
    added operators

---
+++ 03_operator.js
@@ -0,0 +1,55 @@
+
+// + Addition
+// - Subtraction
+// * Multiplication
+console.log(`-------- Exponential ------------`);
+var num1 = 10;
+var num2 = 3;
+var result = num1 ** num2;
+console.log(`Result is : ${result}`);
+
+console.log(`-------- Division ------------`);
+var result = num1 / num2;
+console.log(`Result is : ${result}`);
+
+console.log(`-------- Modulus ------------`);
+var result = num1 % num2;
+console.log(`Result is : ${result}`);
+console.log(` ${15%3}`);
+console.log(`${12%2}`);
+
+console.log(`--------- Assignment Operators -------------`);
+var num1 = 10;
+num1+=5; // num1 + 5;  num1 = 15
+console.log(`Result is : ${num1}`);
+num1-=5;
+console.log(`Result is : ${num1}`);
+num1*=5;
+console.log(`Result is : ${num1}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(`------------------------Assignment Operators-------------------------`);//this is my own created code
+var num1 = 10;
+num1 = num1 + 5 ;
+// var result = num1 +5;
+num1+=5;  //num1 +5;  & num1 = 15
+console.log(`Result is : ${num1}`);
+
+
+// var num2 = 20;
+// num2=+5;  //num 2 = 5; then  num +5; 1st assign the  value the add value
+// console.log(`Result is: ${num2}`);
+// //this methods are not used bcoz its create jumblling
+// num2=-10; // num2 = 10;  num2-10;
+// console.log(`Result is: ${num2}`);
