75728956fa2a0a048fee758ad2f7ff8bb3c583b6
sonaligkhaire <sonaligkhaire601@gmail.com>
2023-07-25 08:39:48 +0530

    commit all operators
    commit all operators

---
+++ 05_ternaryOperator.js
@@ -0,0 +1,13 @@
+
+
+//ternary operator=> condition1(5>=4) ? "I am good girl"(true) : "I am very smart"(false); if condition true then return 1st statement otherwise return 2nd statement.
+
+var jennySsc = 97;
+var jennyHsc = 67;
+//
+var result = (jennySsc >= 60 && jennyHsc>=60) ? "Please go inside all the best" : "Chal Nikal";
+console.log(result);
+
+var num = 50;
+var result = num%2==0 ? "EVEN" : "ODD";
+console.log(result);
