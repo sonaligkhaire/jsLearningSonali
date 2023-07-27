b15f98612f1dafa1d65a56eb1d5c09d8415691c4
sonaligkhaire <sonaligkhaire601@gmail.com>
2023-07-28 02:27:02 +0530

    commit  some implicit conversion facts
    commit  some implicit conversion facts

---
+++ 05_nan0C.js
@@ -0,0 +1,35 @@
+
+
+let result;
+
+result = 0==' ';
+console.log(result); 
+console.log(`------------------------------------------------------------------`);
+
+result = 0=='0';
+console.log(result);
+console.log(`------------------------------------------------------------------`);
+
+
+result = 0==false;
+console.log(result);
+console.log(`------------------------------------------------------------------`);
+
+
+result = null == undefined;
+console.log(result);
+console.log(`------------------------------------------------------------------`);
+
+
+result = 1==[1];
+console.log(result);
+console.log(`------------------------------------------------------------------`);
+
+
+result = 1==true;
+console.log(result);
+console.log(`------------------------------------------------------------------`);
+
+
+result = 1=='1';
+console.log(result);
