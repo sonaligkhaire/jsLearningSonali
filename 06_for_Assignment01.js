93e8f5696ad55e1d869984af0686a85d37533809
sonaligkhaire <sonaligkhaire601@gmail.com>
2023-08-01 00:07:24 +0530

    commit assignment of for loop
    commit assignment of for loop

---
+++ 06_for_Assignment01.js
@@ -0,0 +1,47 @@
+
+//Using for loop only.....................
+
+console.log(`----------------------Assignment no - 1-------------------------------------`);
+
+//print numbers from 5 to 15 by incrementing by  1
+for (let index = 5; index <=15 ; index++) {
+    console.log(index);  
+}
+
+console.log(`-------------------------------------------`);
+//print numbers from 50 to 40 by decrementing by 1
+for (let index = 50; index >=40; index--) {
+    console.log(index);
+}
+
+console.log(`---------------------------------------------`);
+// print first 15 odd numbers
+for (let index = 1; index <30; index= index+2) {
+  console.log(index);
+}
+
+console.log(`---------------------------------------------------`);
+//print first 10 even numbers
+for (let index = 0; index <20; index = index+2) {
+    console.log(index);
+}
+
+console.log(`----------------------------------------------------`);
+//print table of 5
+for (let index = 5; index <=50; index =index+5) {
+    console.log(index);
+    
+}
+
+console.log(`--------------------------------------------------`);
+//print table of 10
+for (let index = 10; index <=100; index = index+10) {
+    console.log(index);
+}
+
+console.log(`------------------------------------------------------`);
+//print table of 10 in reverese order
+for (let index = 100; index >=10; index =index-10) {
+    console.log(index);
+    
+}
