b327fc6287286803247e8b846f088615fd9c4fa9
sonaligkhaire <sonaligkhaire601@gmail.com>
2023-08-01 00:07:49 +0530

    commit Assignment of while loop
    commit Assignment of while loop

---
+++ 06_while_Assignment01.js
@@ -0,0 +1,60 @@
+//Using While loop only..................
+
+
+console.log(`-----------------------Assignment no 2-----------------------------`);
+//print numbers from 5 to 15 incrementing by 1
+var i = 5;
+while (i<= 15) {
+    console.log(i);
+    i++;
+}
+
+
+console.log(`----------------------------------------------------------------------`);
+//print numbers from 50 to 40 decrementing by 1
+var i = 50;
+while (i>=40) {
+    console.log(i);
+    i--;
+}
+
+console.log(`------------------------------------------------------------------------`);
+//print first 15 odd numbers
+var i=1;
+while (i<30) {
+    console.log(i);
+    i = i+2;
+}
+
+
+console.log(`--------------------------------------------------------------------------`);
+//print first 10 even numbers
+var i = 0;
+while (i<20) {
+    console.log(i);
+    i = i+2;
+}
+
+console.log(`------------------------------------------------------------------------------`);
+//print table of 5
+var i = 5;
+while (i<=50) {
+    console.log(i);
+    i = i+5;    
+}
+
+console.log(`-----------------------------------------------------------------------------`);
+//print table of 10
+var i = 10;
+while (i<=100) {
+    console.log(i);
+    i = i+10; 
+}
+
+console.log(`---------------------------------------------------------------------------`);
+//print table of 10 in reverse order
+var i = 100;
+while (i>=10) {
+    console.log(i);
+    i = i-10;
+}
