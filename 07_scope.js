e22c3772af01d146fec5d13955bc37c8757837b2
sonaligkhaire <sonaligkhaire601@gmail.com>
2023-08-02 02:07:04 +0530

    commit variable declaration scope type
    commit variable declaration scope type

---
+++ 07_scope.js
@@ -0,0 +1,42 @@
+
+
+var city; // Variable Declaration //it can acess anywhere
+let street;
+const pin = 443211;
+city = "Pune";
+street = "Wakad Road";
+// pin = "445578"; // Not allowed
+
+var city; // variable re Declaration
+// let street; // variable re Declaration not allowed
+
+if(true) {
+    var namePlayer = "Virat";
+    let salary = 45000;
+    const runs = 33000;
+}
+console.log(namePlayer);
+//console.log(salary);
+// console.log(runs);
+
+function show(){
+    var college = "COEP";
+    let matches = 500;
+    const won = 450; 
+}
+// console.log(college);
+// console.log(matches);
+// console.log(won);
+
+function display(){
+    console.log(`Inside function`);
+    let isMarried = true;
+    if (isMarried) {
+        let greet =  "Good Morning";
+        const PI = 3.1412;
+        console.log(); 
+    }
+    // console.log(PI);
+    // console.log(greet);
+}
+display();
