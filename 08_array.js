ca456366fd3347919ffb6c287eecc54113b8f2ba
sonaligkhaire <sonaligkhaire601@gmail.com>
2023-08-05 01:35:05 +0530

    commit array
    commit array

--- 08_array.js
+++ 08_array.js
@@ -119,3 +119,36 @@
  //4th index se 2 value replace and insert new 4 value
  array.splice(4,2,33,55,66,77);
  console.log(array);
+
+
+
+
+
+
+ console.log(`-----------------concat-----------------------`);
+ let array1 =[3,4,5,6];
+ let array2 = [6,7,8,9];
+ let array3 = array1.concat(array2);
+ console.log(array3);
+
+
+ console.log(`-------------join------------------------------`);
+  let result = array1.join(" ");
+  console.log(result);
+
+
+  console.log(`------------for of loop----------------------`);
+  for (const element of array3) {
+    console.log(element);
+  }
+
+
+  console.log(`---------------array resize-------------------------`);
+  let array4 = [4,5,6,7,8,9];
+  array4.length = 3;
+  console.log(array4);
+
+console.log(`------------------spread operator... ------------------`);
+  let array5 = [4,5,6,7,8,9];
+  console.log(array5);
+  console.log(...array5);
